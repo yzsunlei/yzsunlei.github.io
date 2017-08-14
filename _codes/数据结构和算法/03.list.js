818a49e8d2334b4ac715945e878d80d6f31ae0ae
yzsunlei <sun846990846>
2017-08-14 22:09:12 +0800

    update

--- _codes/数据结构和算法/03.list.js
+++ _codes/数据结构和算法/03.list.js
@@ -6,21 +6,23 @@ function List() {
    this.listSize = 0;
    this.pos = 0;
    this.dataStore = [];
-   this.clear = clear;
-   this.find = find;
-   this.toString = toString;
-   this.insert = insert;
    this.append = append;
    this.remove = remove;
+   this.find = find;
+   this.length = length;
+   this.toString = toString;
+   this.insert = insert;
+   this.clear = clear;
+   this.contains = contains;
    this.front = front;
    this.end = end;
    this.prev = prev;
    this.next = next;
-   this.length = length;
    this.currPos = currPos;
    this.moveTo = moveTo;
    this.getElement = getElement;
-   this.length = length;
+   this.hasNext = hasNext;
+   this.hasPrev = hasPrev;
 }
 
 function append(element) {
@@ -36,6 +38,14 @@ function find(element) {
    return -1;
 }
 
+function length() {
+   return this.listSize;
+}
+
+function toString() {
+    return this.dataStore;
+}
+
 function remove(element) {
    var foundAt = this.find(element);
    if (foundAt > -1) {
@@ -46,14 +56,81 @@ function remove(element) {
    return false;
 }
 
-function toString() {
-    return this.dataStore;
+function insert(element, after) {
+  var insertPos = this.find(after);
+  if (insertPos > -1) {
+     this.dataStore.splice(insertPos + 1, 0 , element);
+     ++this.listSize;
+     return true;
+  }
+  return false;
 }
 
+function clear() {
+   delete this.dataStore;
+   this.dataStore.length = 0;
+   this.listSize = this.pos = 0;
+}
+
+function contains(element) {
+   for (var i = 0; i < this.dataStore.length; i++) {
+      if (this.dataStore[i] == element) {
+         return true;
+      }
+   }
+   return false;
+}
+
+function front() {
+   this.pos = 0;
+}
+
+function end() {
+   this.pos = this.listSize - 1;
+}
+
+function prev() {
+   if (this.pos > 0) {
+      --this.pos;
+   }
+}
+
+function next() {
+   if (this.pos < this.listSize) {
+      ++this.pos;
+   }
+}
+
+function currPos() {
+   return this.pos;
+}
+
+function moveTo(position) {
+   this.pos = position;
+}
+
+function getElement() {
+   return this.dataStore[this.pos];
+}
+
+function hasNext() {
+   return this.pos < this.listSize;
+}
+
+function hasPrev() {
+   return this.pos > 0;
+}
+
+// test
 var names = new List();
 names.append("Cynthia");
 names.append("Raymond");
-names.append("Barbara");
+names.insert("Barbara", "Cynthia");
 console.log(names.toString());
 names.remove("Raymond");
 console.log(names.toString());
+
+// 迭代器访问
+for (names.front(); names.hasNext(); names.next()) {
+   console.log(names.getElement());
+}
--- _codes/数据结构和算法/04.stack.js
+++ _codes/数据结构和算法/04.stack.js
@@ -2,3 +2,50 @@
  * Created by lei.sun on 2017/8/13.
  */
 // 栈
+function Stack() {
+  this.dataStore = [];
+  this.top = 0;
+  this.push = push;
+  this.pop = pop;
+  this.peek = peek;
+  this.clear = clear;
+  this.length = length;
+}
+
+function push(element) {
+  this.dataStore[this.top++] = element;
+}
+
+function peek() {
+  return this.dataStore[this.top - 1];
+}
+
+function pop() {
+  return this.dataStore[--this.top];
+}
+
+function clear() {
+  this.top = 0;
+}
+
+function length() {
+  return this.top;
+}
+
+// test
+var s = new Stack();
+s.push("Cynthia");
+s.push("Raymond");
+s.push("Barbara");
+console.log("length:" + s.length());
+//console.log(this.peek());
+var poped = s.pop();
+console.log("The poped element is: " + poped);
+//console.log(this.peek());
+s.clear();
+console.log("length:" + s.length());
+
+// 更多实例
+// 进制间相互转换
+// 回文判断
+// 递归演示
